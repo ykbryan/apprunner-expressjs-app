fed52c0fa6dea78e85e1e0715f26b5bdb248b3e3
Bryan Chua <me@bryanchua.com>
2021-07-06 16:34:56 +0800

    redeploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send(`Hello to AWS AppRunner at port ${port}!`)
+  res.send(`Hello to AWS AppRunner at port ${port} !!!`)
 })
 
 app.listen(port, () => {
